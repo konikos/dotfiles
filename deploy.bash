80fbc35dc0596cb440278de9dc5a53577b0abe14
Nikos Kokolakis <kokolakisnikos@gmail.com>
2014-08-22 01:47:15 +0400

    deploy script

---
+++ deploy.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+REPO=git@github.com:konikos/dotfiles.git
+DEPLOY_SCRIPT=deploy.bash
+
+TEMPDIR=$(mktemp -d)
+git clone "$REPO" "$TEMPDIR"
+
+cd "$TEMPDIR"
+git submodule update --init --recursive
+git update-index --assume-unchanged "$DEPLOY_SCRIPT"
+rm -f "$DEPLOY_SCRIPT"
+
+cd "$HOME"
+rsync -avr "${TEMPDIR}/" "${HOME}/"
+rm -rf "$TEMPDIR"
+
+echo "dotfiles deployed, enjoy."
+
