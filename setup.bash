c98ed528daeff05efdb2cbb54449ab26646e1057
Nikos Kokolakis <kokolakisnikos@gmail.com>
2019-02-08 12:36:28 +0100

    Backup a existing dotfile's full path

--- setup.bash
+++ setup.bash
@@ -24,16 +24,45 @@ mkbackup_dir() {
 	fi
 }
 
-# backup_if_exists BACKUP_DIR DOTFILE
-backup_if_exists() {
-	local -r backup_dir=$1
-	local -r dotfile=$2
+abspath() {
+	echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+}
 
-	if [[ ! -e "$dotfile" ]]; then
+# backup_if_exists backup_dir path
+backup_if_exists() {
+	local backup_dir src dest
+
+	backup_dir=$1
+	src=$2
+	dest="${backup_dir}/$(abspath "$src" | sed 's|^/\+||')"
+
+	if ! [[ -e $src ]]; then
+		return
+	fi
+
+	mkdir -p "$(dirname "$dest")"
+	mv "$src" "$dest"
+}
+
+# link_dotfile backup_dir source_path target_path
+link_dotfile() {
+	local backup_dir source_path target_path
+
+	backup_dir=$1; shift
+	source_path=$1; shift
+	target_path=$1; shift
+
+	if [[ ! -e $source_path ]]; then
 		return 0
 	fi
 
-	mv "$dotfile" "${backup_dir}/"
+	if [[ "$(readlink "$source_path" || true)" = "$target_path" ]]; then
+		# path is already linked
+		return 0
+	fi
+
+	backup_if_exists "${backup_dir}" "$target_path"
+	ln -s "$source_path" "$target_path"
 }
 
 main() {
@@ -41,6 +70,7 @@ main() {
 	if ! backup_dir=$(mkbackup_dir); then
 		die "Could not create backup directory"
 	fi
+	log "Will backup existing files to \`$backup_dir'."
 
 	local -r dotfiles_dir="${HOME}/.dotfiles"
 	backup_if_exists "$backup_dir" "$dotfiles_dir"
@@ -48,19 +78,19 @@ main() {
 
 	(cd "$dotfiles_dir" && git submodule update --init --recursive)
 
-	local dotfile rel_dir target_dir link_source
+	local rel_dotfile git_dotfile rel_dir target_dir home_dotfile
 	git -C "$dotfiles_dir" ls-tree --full-tree -r HEAD \
 		| awk '{ print $4 }' \
-		| while read -r dotfile; do
-			rel_dir=$(dirname "$dotfile")
+		| while read -r rel_dotfile; do
+			rel_dir=$(dirname "$rel_dotfile")
 			target_dir="${HOME}/${rel_dir}"
 			if ! mkdir -p "${target_dir}"; then
 				die "Could not create directory ${target_dir}"
 			fi
 
-			link_source="${HOME}/${dotfile}"
-			backup_if_exists "$backup_dir" "$link_source"
-			ln -s "${dotfiles_dir}/$dotfile" "$link_source"
+			git_dotfile="${dotfiles_dir}/${rel_dotfile}"
+			home_dotfile="${HOME}/${rel_dotfile}"
+			link_dotfile "$backup_dir" "$git_dotfile" "$home_dotfile"
 	done
 
 	for f in "${USELESS_FILES[@]}"; do
@@ -68,7 +98,9 @@ main() {
 	done
 
 	if ! rmdir "$backup_dir" >/dev/null 2>&1; then
-		log "Existing files were backed up to \`${backup_dir}'"
+		log "Existing files were backed up to \`${backup_dir}'."
+	else
+		log "No backups were required."
 	fi
 
 	log "Don't forget to run \`[nv]im-update'."
